caff3d45b56e9686f7493e47cc4a0da272c93a9e
Cristian-Putra <cistianputra626@gmail.com>
2025-05-24 23:55:39 +0700

    Pertemuan 3 : Operasi Kondisi

--- Pertemuan 3/Latihan.cpp
+++ Pertemuan 3/Latihan.cpp
@@ -3,17 +3,17 @@ using namespace std;
 
 int main() {
     system("cls");
-    int bilangan;
+    int bilangan; // deklarasi variabel bilangan dengan tipe data integer
 
  
     cout << "Masukkan sebuah bilangan bulat: ";
     cin >> bilangan;
 
-    if (bilangan % 10 == 0) {
+    if (bilangan % 10 == 0) { // apakah bilangan(yang telah diinputkan) memiliki sisa bagi 0 setelah dibagi dengan 10
         cout << bilangan << " adalah kelipatan dari 10." << endl;
-    } else if (bilangan % 5 == 0) {
+    } else if (bilangan % 5 == 0) { // apakah bilangan(yang telah diinputkan) memiliki sisa bagi 0 setelah dibagi dengan 5
         cout << bilangan << " adalah kelipatan dari 5." << endl;
-    } else {
+    } else { // bilangan bukan merupakan sisa bagi dari 5 ataupun 10
         cout << bilangan << " bukan kelipatan dari 5 atau 10." << endl;
     }
 
