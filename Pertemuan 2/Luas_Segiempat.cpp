18824b8e59aaedc38f827793ed46a039b0024ba4
Cristian-Putra <cistianputra626@gmail.com>
2025-05-24 23:37:12 +0700

    Pertemuan 2 : Penggunaan Operator

--- Pertemuan 2/Luas_Segiempat.cpp
+++ Pertemuan 2/Luas_Segiempat.cpp
@@ -1,15 +1,15 @@
-#include<iostream>
-using namespace std;
+#include<iostream> //header cpp
+using namespace std; // digunakan agar kita tidak perlu mengetikkan std berulang di dalam program
 
-int main()
+int main() // indikasi program utama
 {
     system("cls");
-    float luas, p, l;
+    float luas, p, l; // mendeklarasikan variabel luas,p(panjang).l(lebar) dengan tipe data float
 
-    cout << "Masukkan Panjang Segiempat : ";cin>>p;
-    cout << "Masukkan Lebar Segiempat : ";cin>>l;
+    cout << "Masukkan Panjang Segiempat : ";cin>>p; //user diminta memasukkan nilai panjang dan kemudian nilainya akan disimpan 
+    cout << "Masukkan Lebar Segiempat : ";cin>>l; // user dimita memasukkan nilai lebar dan kemudian nilainya akan disimpan
     
-    luas = p * l;
-    cout << "Luas Segiempat : "<<luas;
+    luas = p * l; // rumus luas persegi
+    cout << "Luas Segiempat : "<<luas; // menampilkan luas segiempat berdasarkan inputan user
     
 }
