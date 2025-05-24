146b9247be5aebdc67deecd1cfc29ca91e6e5e7c
Cristian-Putra <cistianputra626@gmail.com>
2025-05-24 23:30:46 +0700

    Pertemuan 2 : Penggunaan Operator

--- Pertemuan 2/Latihan.cpp
+++ Pertemuan 2/Latihan.cpp
@@ -5,11 +5,11 @@ int main()
 {
     system ("cls");
     float jari_jari, volume, luas_permukaan;
-    const float phi = 3.14;
+    const float phi = 3.14; // konstanta, nilai yang akan selalu tetap
 
-    cout<< "masukkan jari jari bola: ";cin>>jari_jari;
-    volume = (float) (4)/(float) (3)*phi*jari_jari*jari_jari*jari_jari;
-    luas_permukaan = 4*phi*jari_jari*jari_jari;
-    cout<< "volume bola: "<<volume<<endl;
-    cout<< "luas permukaan bola: "<<luas_permukaan<<endl;
+    cout<< "masukkan jari jari bola: ";cin>>jari_jari; // meminta inputan dari user dan menyimpannya 
+    volume = (float) (4)/(float) (3)*phi*jari_jari*jari_jari*jari_jari; //rumus untuk volume bola
+    luas_permukaan = 4*phi*jari_jari*jari_jari; // rumus untuk luas permukaan bola
+    cout<< "volume bola: "<<volume<<endl; // menampilkan volume bola berdasarkan hasil inputan user
+    cout<< "luas permukaan bola: "<<luas_permukaan<<endl; // menampilkan luas permukaan bola berdasarkan inputan user
 }
