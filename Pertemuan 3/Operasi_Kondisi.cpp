6713b5507836ef272ea52caae684c85aa79104a4
Cristian-Putra <cistianputra626@gmail.com>
2025-05-25 00:16:45 +0700

    Pertemuan 3 : Operasi Kondisi

--- Pertemuan 3/Operasi_Kondisi.cpp
+++ Pertemuan 3/Operasi_Kondisi.cpp
@@ -4,17 +4,17 @@ int main()
 {
     system("cls");
     
-    int nilai;
+    int nilai; // deklarasi variabel nilai dengan tipe data integer
 
     cout<<"Masukan nilai : ";
     cin>>nilai;
 
-    //if statement
+    //if statement, statement yang digunakan untuk memastikan apakah kondisi yang ada memenuhi syarat yang diberikan
    /* if (nilai <=65){
         cout<<"Anda tidak lulus"<<endl;
     }*/
 
-    //if else statement
+    //if else statement, memiliki dua buah kondisi
     /*if (nilai <=65){
         cout<<"Anda tidak lulus"<<endl;
     }
@@ -22,7 +22,7 @@ int main()
         cout<<"Anda lulus"<<endl;
     }*/
 
-    // if else if statement
+    // if else if statement, statement yang menampung kondisi sebanyak 3 atau lebih
    /* if (nilai == 100){
         cout <<"Anda keceh !"<<endl;
     }
@@ -33,21 +33,21 @@ int main()
         cout<<"Anda lulus"<<endl;
     }*/
 
-    // Nested if
-    /*if (nilai <= 65){
+    // Nested if // kondisi dimana ada kondisi di dalam kondisi
+    /*if (nilai <= 65){ , (apakah nilainya kurang dari sama dengan 65)
         cout <<" Anda tidak lulus "<<endl;
-    }else {
-        if (nilai == 100){
+    }else { (selain dari nilai 65 ke bawah)
+        if (nilai == 100){ (jika nilai selain 65 tadi adalah 100)
             cout <<"Anda lulus dan anda keceh" <<endl;
-        }else{
+        }else{ (jika nilai selain 65 tadi adalah selain dari 100)
             cout <<"Anda lulus"<<endl;
         }
     }*/
 
-    //switch case
+    //switch case, statement untuk kondisi untuk memilih salah satu dari kondisi yang disediakan 
    /* switch (nilai){
         case 1:
-             cout << "Senin" <<endl;break;
+             cout << "Senin" <<endl;break; (menggunakan sintaks break untuk menghentikan statement pemilihan agar tidak lanjut ke pilihan selanjutnya)
         case 2:
              cout << "Selasa" <<endl;break;
         case 3:
@@ -60,10 +60,10 @@ int main()
              cout << "Sabtu" <<endl;break;   
         case 7:
              cout << "Minggu" <<endl;break;
-        default: cout << "inputan tidak valid"<<endl;break;     
+        default: cout << "inputan tidak valid"<<endl;break; (default, sintaks yang digunakan ketika tidak ada pilihan dari opsi yang disediakan)     
     }*/
 
-    //switch range
+    //switch range, pemilihan kondisi dari rentang nilai yang ada 
    /* switch (nilai){
         case 85 ... 100 : cout << "A" <<endl;break;
         case 80 ... 84 : cout << "B+" <<endl;break;
@@ -74,13 +74,13 @@ int main()
         default: cout<<"E"<<endl;break;
     }*/
 
-    //Ternary Operator
-    /*if (nilai %2 == 0){
+    //Ternary Operator, gabungan dari operasi kondisi dengan penggunaan operator aritmatika
+    /*if (nilai %2 == 0){ (apakah nilai(hasil inputan user) habis dibagi 2)
         cout << "Genap nih"<< endl;
     } else {
         cout << "Ganjil sih" << endl;
     }*/
 
-    string checknum = (nilai % 2 == 0) ? "Genap" : "Ganjil";
+    string checknum = (nilai % 2 == 0) ? "Genap" : "Ganjil"; // versi sederhana dari operasi if else
     cout <<nilai<< " tuh bilangan "<<checknum<< " sih"<< endl;
 }
