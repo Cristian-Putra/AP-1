44c8b66b2ed1bdc5544843641858e17da7d56ce5
Cristian-Putra <cistianputra626@gmail.com>
2025-05-25 14:14:14 +0700

    Pertemuan 5 : Array & Vector

--- Pertemuan 5/vektor.cpp
+++ Pertemuan 5/vektor.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main(){
     // deklarasi vektor dan inisialisasi
 
-    vector<string> nama_karyawan = {"budi","martin","jungkok","kimjong","pessi"};
+    vector<string> nama_karyawan = {"budi","martin","jungkok","kimjong","pessi"}; 
 
-    /*for (int i = 0; i < nama_karyawan.size(); i++){
+    /*for (int i = 0; i < nama_karyawan.size(); i++){ // perulangan untuk menampilkan nama karyawan dari indeks 0 sampai indeks 4
         cout << nama_karyawan[i] << endl;
     }
 
@@ -16,14 +16,14 @@ int main(){
     }*/
 
     //add data to vector
-   nama_karyawan.push_back("imam");
+   nama_karyawan.push_back("imam"); // push_back berguna untuk menambahkan nama imam ke dafta nama karyawan pada bagian akhir
     /*for (int i = 0; i < nama_karyawan.size(); i++){
         cout << nama_karyawan[i] << endl;
     }*/
 
     // delete data from vector
-    nama_karyawan.pop_back();
-    nama_karyawan.erase(nama_karyawan.begin() + 3);
+    nama_karyawan.pop_back(); // menghapus nama terakhir dari daftar nama karyawan
+    nama_karyawan.erase(nama_karyawan.begin() + 3); // menghapus nama indeks ke 3 pada daftar nama karyawan
      for (int i = 0; i < nama_karyawan.size(); i++){
         cout << nama_karyawan[i] << endl;
     }
