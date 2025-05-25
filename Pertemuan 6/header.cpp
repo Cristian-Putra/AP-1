e91c5bb4018667590c3e71750d13f452c8c51c43
Cristian-Putra <cistianputra626@gmail.com>
2025-05-25 15:38:02 +0700

    Pertemuan 6 : Fungsi dan Header

--- Pertemuan 6/header.cpp
+++ Pertemuan 6/header.cpp
@@ -8,18 +8,18 @@ int jumlah (int a, int b){
 }
 
 int kurang (int a, int b){
-    r a - b;
+    r a - b; // r mengganti return
 }
 
-pessi{
-    decul;
+pessi{ // pessi menggantikan int main()
+    decul; // decul menggantikan system("cls")
     int n;
 
-    o l jumlah (2, 3) << e;
+    o l jumlah (2, 3) << e; // o sebagai cout, l sebagai tanda << , e sebagai endl
     o l kurang (5, 3) << e;
 
-    skibidi rizz = 25.0;
-    skibidi siuuu = sqrt(angka);
+    skibidi rizz = 25.0; // skibidi menggantikan tipe data float, rizz menggantikan variabel angka
+    skibidi siuuu = sqrt(angka); // siuuu menggantikan variabel akar
     o << "akar dari " << angka << " adalah " << akar << endl;
 
 }
