ef2675e76b4c541e115ff7b77a167ccfe418107f
Cristian-Putra <cistianputra626@gmail.com>
2025-05-25 15:40:37 +0700

    Pertemuan 6 : Fungsi dan Header

--- Pertemuan 6/variabel.cpp
+++ Pertemuan 6/variabel.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 using namespace std;
 
-string nama_global = "Ilmu";
+string nama_global = "Ilmu"; // variabel global
 
 void nama_variabel(){
-    string nama_lokal = "Komputer";
+    string nama_lokal = "Komputer"; // variabel lokal
 
     cout <<nama_lokal << endl;
 
     cout << nama_global << endl;
 }
-int main(){
+int main(){ // program utama
     system("cls");
     nama_variabel();
     cout << nama_global << endl; 
