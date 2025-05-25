94d0490e9a4635533733e8e8e4c5831bd49d9ede
Cristian-Putra <cistianputra626@gmail.com>
2025-05-25 15:15:19 +0700

    Pertemuan 6 : Fungsi dan Header

--- Pertemuan 6/belajar_fungsi.cpp
+++ Pertemuan 6/belajar_fungsi.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 
-void sapa(string nama){
+void sapa(string nama){ //fungsi untuk menerima parameter nama bertipe data string
     system("cls");
 
     cout << "Halo " << nama << "! Selamat belajar C++! "<< endl;
 }
-int main(){
-    string nama_pengguna = "Tian";
+int main(){ // fungsi utama
+    string nama_pengguna = "Tian"; // menginisialisasi nama_pengguna dengan value Tian 
 
-    sapa(nama_pengguna);
+    sapa(nama_pengguna); // memanggil fungsi sapa
 
     return 0;
 }
