0c83828219af1cf782610178b9ad788a4d4eff2a
Cristian-Putra <cistianputra626@gmail.com>
2025-05-25 15:11:49 +0700

    Pertemuan 6 : Fungsi dan Header

--- Pertemuan 6/headerCustom.h
+++ Pertemuan 6/headerCustom.h
@@ -1,10 +1,10 @@
-#define u using namespace std;
-#define l <<
-#define o cout
-#define e endl
-#define decul system("cls")
-#define r return
-#define skibidi double
-#define rizz angka
-#define siuuu akar
-#define pessi int main()
+#define u using namespace std; // mengganti tulisan using namespace std; dengan kata u tetapi masih mempunyai fungsi using namespace std;
+#define l << // mengganti tanda << menjadi karakter l
+#define o cout // mengganti kata cout menjadi o
+#define e endl // mengganti kata endl menjadi e
+#define decul system("cls") // mengganti kata system("cls") menjadi secul
+#define r return // mengganti  kata return menjadi r
+#define skibidi double // mengganti kata double menjadi skibidi
+#define rizz angka // mengganti kata angka menjadi rizz
+#define siuuu akar //mengganti kata akar menjadi siuu
+#define pessi int main() // mengganti int main() menjadu pessi
